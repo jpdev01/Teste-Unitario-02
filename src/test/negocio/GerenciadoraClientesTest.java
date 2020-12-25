af74822142832a10a9f50c2eaa8fb095de40a311
João Pedro Truchinski Borba <jptruchinski@gmail.com>
2020-12-25 16:41:14 -0300

    new test to GerenciadoraClientes.removeCliente()

--- src/test/negocio/GerenciadoraClientesTest.java
+++ src/test/negocio/GerenciadoraClientesTest.java
@@ -14,8 +14,8 @@ public class GerenciadoraClientesTest {
     }
 
     @Test
-    @DisplayName("Testa se a função pesquisa cliente está trazendo o cliente correto com base no ID")
-    @Tag("testCalculator")
+    @DisplayName("Verifica se a função pesquisa cliente está trazendo o cliente correto com base no ID")
+    @Tag("testGerenciadoraClientes")
     public void testPesquisaCliente(){
 
         //Arrange
@@ -33,6 +33,29 @@ public class GerenciadoraClientesTest {
 
         //Assert
         Assertions.assertEquals(searchResult.getId(), c1.getId());
+    }
+
+    @Test
+    @DisplayName("Verifica se a função remove cliente está removendo e removendo o cliente correto")
+    @Tag("testGerenciadoraClientes")
+    public void removeCliente(){
+        //Arrange
+        Cliente c1 = new Cliente (1, "João Pedro", 17, "joao@neo", 1, true);
+        Cliente c2 = new Cliente (2, "Victor Eduardo", 18, "victor@neo", 1, true);
+
+        List<Cliente> clientesDoBanco = new ArrayList<>();
+        clientesDoBanco.add(c1);
+        clientesDoBanco.add(c2);
+
+        GerenciadoraClientes gerClientes = new GerenciadoraClientes(clientesDoBanco);
+
+        //Act
+        boolean removeCliente = gerClientes.removeCliente(1);
+
+        //Assert
+        Assertions.assertEquals(removeCliente, true);
+        Assertions.assertEquals(gerClientes.getClientesDoBanco().size(), 1);
+        Assertions.assertNull(gerClientes.pesquisaCliente(1));
     }
 
 
